3ba640b4db399b7b6e71c551f11895849d91727a
armin.buch@gmx.de <armin.buch@gmx.de@b32817a8-963e-0410-adbb-89e8b519432e>
2007-11-16 13:07:21 +0000

    tried to add server and file handler into the controller (more comments in mail)

--- trunk/gralej/src/gralej/controller/Controller.java
+++ trunk/gralej/src/gralej/controller/Controller.java
@@ -3,9 +3,9 @@ package gralej.controller;
 import java.io.*;
 
 import gralej.*;
-import gralej.fileIO.*;
-import gralej.gui.*;
+import gralej.fileIO.FileLoader;
 import gralej.parsers.*;
+import gralej.testers.DummyStreamHandler;
 import gralej.server.*;
 
 /**
@@ -24,13 +24,52 @@ public class Controller {
 	
 	private ContentModel cm; // 
 	
+	private IGraleServer server;
+	
+	private INewStreamListener listener;
+		
+	private IParseResultReceiver receiver; // listens to parse results
+	
 	
 	public void open (File file) {
+		
+		// old way: let opening be handled by the model
+		// TODO postpone this until parse is received
+		// TODO make it independent from a File (currently only used for naming the window)
 		cm.open(file);
+
+		
+		// new way: instantiate a new file handler
+		System.err.println("-- Opening File " + file.getAbsolutePath());
+		FileLoader fl = new FileLoader(file, true);
+		fl.registerNewStreamListener(listener);
+		System.err.println("-- Starting to open");
+		try {
+			fl.loadFile();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		System.err.println("-- Opening should run in its thread now");		
 		
 	}
 	
+	public void notifyOfStream(InputStream s, String type) {
+		System.err.println("-- Controller got new stream of type " + type);				
+		
+		IGraleParser parser;
+		try {
+			// ask parser factory for parser
+			parser = GraleParserFactory.createParser(type);
+			// plug stream into parser, and wait for results
+			parser.parse(s, receiver);
+		} catch (UnsupportedProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	public void close () {
+		// notify content model of change. to this change in the cm, the gui listens
 		cm.close();		
 	}
 	
@@ -38,8 +77,15 @@ public class Controller {
 		return cm;
 	}
 
-	public Controller() {
+	public Controller(IGraleServer server) {
+		this.server = server;
+		listener = new StreamHandler(this);
+		server.registerNewStreamListener(listener);
+		
 		cm = new ContentModel();
+		
+		// instantiate parse result receiver
+		
 	}
 
 }
