df320a197c9d9ae82fc6682154b87828e1da830c
niels <niels@drni.de>
2008-01-20 13:55:55 +0000

    added L+F care to prefs dialog tester

--- gralej/src/gralej/testers/PrefsDialogester.java
+++ gralej/src/gralej/testers/PrefsDialogester.java
@@ -1,7 +1,10 @@
 package gralej.testers;
 
 import gralej.gui.prefsdialog.GenDialog;
+import gralej.prefs.GralePreferences;
 import gralej.prefs.GralePrefsInitException;
+
+import javax.swing.UIManager;
 
 /**
  * @version $Id$
@@ -11,6 +14,19 @@ import gralej.prefs.GralePrefsInitException;
 public class PrefsDialogester {
 
     public static void main(String[] args) throws GralePrefsInitException {
+    	
+    	try {
+			UIManager.setLookAndFeel(GralePreferences.getInstance().get("gui.l+f.java-l+f"));
+		} catch (Exception e) {
+			try {
+				UIManager.setLookAndFeel(
+						UIManager.getSystemLookAndFeelClassName());
+			} catch (Exception e1) {
+				throw new RuntimeException("Cannot set system default L&F!", e1);
+			}
+			System.err.println("Falling back to default L&F");
+			e.printStackTrace();
+		}	
 
         // JFrame frame = new PrefDialogFrame();
         GenDialog frame = new GenDialog(null);
