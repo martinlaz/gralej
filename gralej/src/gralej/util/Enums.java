c55728bc8f5a459e27800969b8bfd4504538a4d8
martinlaz <martinlaz@b32817a8-963e-0410-adbb-89e8b519432e>
2008-03-04 18:33:49 +0000

    Decoding of enums from ordinals in gralej.util.Enums

--- gralej/src/gralej/util/Enums.java
+++ gralej/src/gralej/util/Enums.java
@@ -5,26 +5,15 @@
 
 package gralej.util;
 
-import java.lang.reflect.Field;
-
 /**
  *
  * @author Martin
  */
 public class Enums {
     public static <T extends Enum<T>> T valueOf(Class<T> enumType, int ordinal) {
-        for (Field f : enumType.getFields()) {
-            if (f.isEnumConstant()) {
-                T e;
-                try {
-                    e = (T) f.get(null);
-                }
-                catch (IllegalAccessException ex) {
-                    continue;
-                }
-                if (e.ordinal() == ordinal)
-                    return e;
-            }
+        for (T e : enumType.getEnumConstants()) {
+            if (e.ordinal() == ordinal)
+                return e;
         }
         throw new IllegalArgumentException(
             "Cannot find an enum with ordinal " + ordinal + " in " + enumType);
