b347b80fd3815550a2b87bbc2514bf47f69e1f0d
martinlaz <martinlaz@b32817a8-963e-0410-adbb-89e8b519432e>
2008-10-26 13:30:28 +0000

    License and disclamer header

--- gralej/src/gralej/blocks/configurator/BlockConfiguratorDialog.java
+++ gralej/src/gralej/blocks/configurator/BlockConfiguratorDialog.java
@@ -1,7 +1,25 @@
 /*
- * BlockConfiguratorDialog.java
+ *  $Id$
  *
- * Created on 6. März 2008, 12:50
+ *  Author:
+ *     Martin Lazarov [mlazarov at sfs.uni-tuebingen.de]
+ *     
+ *  This file is part of the Gralej system
+ *     http://code.google.com/p/gralej/
+ *
+ *  Gralej is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Gralej is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 package gralej.blocks.configurator;
@@ -13,7 +31,6 @@ import java.awt.BorderLayout;
 import javax.swing.UIManager;
 
 /**
- *
  * @author  Martin
  */
 public class BlockConfiguratorDialog extends javax.swing.JDialog implements BlockConfigurator.Handler {
--- gralej/src/gralej/util/Enums.java
+++ gralej/src/gralej/util/Enums.java
@@ -1,11 +1,29 @@
-// $Id$
+/*
+ *  $Id$
+ *
+ *  Author:
+ *     Martin Lazarov [mlazarov at sfs.uni-tuebingen.de]
+ *     
+ *  This file is part of the Gralej system
+ *     http://code.google.com/p/gralej/
+ *
+ *  Gralej is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  Gralej is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
 package gralej.util;
 
-/**
- *
- * @author Martin
- */
 public class Enums {
     public static <T extends Enum<T>> T valueOf(Class<T> enumType, int ordinal) {
         for (T e : enumType.getEnumConstants()) {
