7810df93eb6d789069044b3deb767ac6f67402a4
martinlaz <martinlaz@b32817a8-963e-0410-adbb-89e8b519432e>
2009-05-14 11:15:48 +0000

    Gralej (0.6.0)

--- trunk/gralej/src/gralej/Globals.java
+++ trunk/gralej/src/gralej/Globals.java
@@ -28,8 +28,8 @@ import java.awt.Cursor;
 
 public class Globals {
     public static final int VERSION_MAJOR = 0;
-    public static final int VERSION_MINOR = 5;
-    public static final int VERSION_REVISION = 4;
+    public static final int VERSION_MINOR = 6;
+    public static final int VERSION_REVISION = 0;
     
     public static final String VERSION_STRING =
             VERSION_MAJOR + "." + VERSION_MINOR + "." + VERSION_REVISION;
